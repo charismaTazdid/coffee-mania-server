e2ee9ac9778d651322f035fcf36ab89386f5429e
raihantazdid <raihantajdid@gmail.com>
2024-03-16 23:57:39 +0600

    new controller with params

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! So! You are watching??';
+    return 'Hello Nestjs?';
   }
 }
--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get()
-  findAll() {
-    return 'This Action will return all The coffee';
+  @Get() // Decorator
+  findAllMenue() {
+    return 'this action will return all Menue';
+  }
+  @Get('list')
+  findAllCoffee() {
+    return 'this action will return all the coffee form coffee list';
+  }
+  // Dynamic search
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This will search and return single coffee from coffee list which id is ${id}`;
+  }
+
+  @Post()
+  createCoffee(@Body() body) {
+    return { message: 'I am inserting your Data', data: body };
   }
 }
