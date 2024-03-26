6464d8d8f87a7c6522c1298d599d80e078878fc0
raihantazdid <raihantajdid@gmail.com>
2024-03-27 00:48:36 +0600

    query and params recap

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,6 +8,7 @@ import {
   Param,
   Patch,
   Post,
+  Query,
   Res,
 } from '@nestjs/common';
 
@@ -22,23 +23,34 @@ export class CoffeesController {
   findAllCoffee() {
     return 'this action will return all the coffee form coffee list';
   }
+
+  // using Query
+  @Get('query')
+  findAll2(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return `This action will search and return particuller coffees, limit: ${limit}, offset: ${offset}`;
+  }
+
   // Dynamic search
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This will search and return single coffee from coffee list which id is ${id}`;
   }
 
+  // Post a Coffeee
   @Post()
   @HttpCode(HttpStatus.GONE) // we can change status code by this line of code
   createCoffee(@Body() body) {
     return { message: 'I am inserting your Data', data: body };
   }
 
+  // Update Data
   @Patch(':id')
   updateCoffe(@Param('id') id: string, @Body() body) {
     return `this action will update the coffee id with: ${id} and updatable data is the ${body}`;
   }
 
+  // Delete a Coffee with Id
   @Delete(':id')
   deleteCoffee(@Param('id') id: string) {
     return `This action will delete the coffee which id is ${id}`;
